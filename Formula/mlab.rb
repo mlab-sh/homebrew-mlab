e3ec472b66498d223878a54a0a3a98d2d8f8dd75
Alice Snow <snow.alice@pm.me>
2025-02-11 22:16:22 +0100

    patch

--- Formula/mlab.rb
+++ Formula/mlab.rb
@@ -1,7 +1,7 @@
 class Mlab < Formula
   desc "This is the official client for mlab.sh"
   homepage "https://mlab.sh"
-  url "https://github.com/mlab-sh/mlab-cli/releases/download/untagged-7c8333b063b44a76a937/mlab-macOS-arm64.tar.gz"
+  url "https://github.com/mlab-sh/mlab-cli/releases/download/v0.0.1/mlab-macOS-arm64.tar.gz"
   sha256 "2bcb71798533867933c742f9f882e476891de96ec4f6df125babef7b153aea25"
   license "MIT"
 
